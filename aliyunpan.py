09c79d8438756e6ea5e390c53715a2849d58eea9
thsrite <thsrite@gmail.com>
2024-09-18 09:07:02 +0800

    推送文件大小

--- aliyunpan.py
+++ aliyunpan.py
@@ -110,6 +110,32 @@ class AliyunPan:
             logger.info(f"开始删除本地文件 {self.__folder_json}")
         self.sync_aliyunpan()
 
+    @staticmethod
+    def str_filesize(size: Union[str, float, int], pre: int = 2) -> str:
+        """
+        将字节计算为文件大小描述（带单位的格式化后返回）
+        """
+        if size is None:
+            return ""
+        size = re.sub(r"\s|B|iB", "", str(size), re.I)
+        if size.replace(".", "").isdigit():
+            try:
+                size = float(size)
+                d = [(1024 - 1, 'K'), (1024 ** 2 - 1, 'M'), (1024 ** 3 - 1, 'G'), (1024 ** 4 - 1, 'T')]
+                s = [x[0] for x in d]
+                index = bisect.bisect_left(s, size) - 1
+                if index == -1:
+                    return str(size) + "B"
+                else:
+                    b, u = d[index]
+                return str(round(size / (b + 1), pre)) + u
+            except ValueError:
+                return ""
+        if re.findall(r"[KMGTP]", size, re.I):
+            return size
+        else:
+            return size + "B"
+
     def sync_aliyunpan(self):
         '''
         同步阿里云文件
@@ -146,7 +172,7 @@ class AliyunPan:
             # 组织msg
             new_file_msg = ""
             for index, file in enumerate(self.__new_files):
-                new_file_msg += f"备份文件 {file.get('name')} \n" \
+                new_file_msg += f"备份文件 {file.get('name')} {self.str_filesize(file.get('size'))}\n" \
                                 f"备份时间 {file.get('time')} \n" \
                                 f" \n"
             # 发送微信通知
